dea068cc9a14bf84a4bcb1d376bc1b4b3e5335f3
arexe <arexepro@gmail.com>
2019-03-15 14:39:36 +0100

    Add app properties and simple index page

---
+++ src/main/java/com/arexe/portal/mainpages/MainPageController.java
@@ -0,0 +1,16 @@
+package com.arexe.portal.mainpages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.ws.rs.GET;
+
+@Controller
+public class MainPageController {
+
+    @GET
+    @RequestMapping(value = {"/", "/index"})
+    public String showMainPage(){
+        return "index";
+    }
+}
