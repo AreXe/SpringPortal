0475c7ae709bd2ed9b9f04fc9008bb4703625aa7
arexe <arexepro@gmail.com>
2019-06-07 10:43:43 +0200

    Add some tests for BookService

---
+++ src/test/java/com/arexe/portal/service/BooksServiceTest.java
@@ -0,0 +1,69 @@
+package com.arexe.portal.service;
+
+import com.arexe.portal.entity.Book;
+import com.arexe.portal.repository.BooksRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class BooksServiceTest {
+
+    @Autowired
+    private BooksRepository repository;
+    @Autowired
+    private BooksService service;
+
+    @BeforeEach
+    void init() {
+        repository.deleteAll();
+    }
+
+    @Test
+    void bookListShouldBeEmptyOnStart() {
+        //when
+        List<Book> bookList = service.getBookList();
+        //then
+        assertTrue(bookList.isEmpty());
+    }
+
+    @Test
+    void bookListShouldContainNewBookAfterAdding() {
+        //given
+        String expectedIsbn = "123";
+        String expectedAuthor = "Author";
+        String expectedTitle = "Book";
+        Book book = Book.builder()
+                .isbn("123")
+                .author("Author")
+                .title("Book")
+                .build();
+        service.saveBook(book);
+        //when
+        List<Book> bookList = service.getBookList();
+        //then
+        Book bookFromList = bookList.get(0);
+        assertEquals(expectedIsbn, bookFromList.getIsbn());
+        assertEquals(expectedAuthor, bookFromList.getAuthor());
+        assertEquals(expectedTitle, bookFromList.getTitle());
+    }
+
+    @Test
+    void shouldReturnEmptyListAfterDeletingExistingBook() {
+        //given
+        Book existingBook = Book.builder().title("Book").build();
+        service.saveBook(existingBook);
+        //when
+        service.deleteBookById(existingBook.getId());
+        List<Book> bookList = service.getBookList();
+        //then
+        assertTrue(bookList.isEmpty());
+    }
+
+}
