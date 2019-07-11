97dc7ffde02b1f58230fab8f8234a393d75c876b
arexe <arexepro@gmail.com>
2019-07-11 21:23:52 +0200

    Update db properties and schema

--- src/main/resources/database/portal.sql
+++ src/main/resources/database/portal.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Czas generowania: 05 Kwi 2019, 12:19
--- Wersja serwera: 10.1.36-MariaDB
--- Wersja PHP: 7.2.11
+-- Host: asgard-mysql
+-- Generation Time: Jul 11, 2019 at 07:06 PM
+-- Server version: 8.0.16
+-- PHP Version: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,54 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `bgames`
+-- Database: `bgames`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `role`
+-- Table structure for table `board_game`
+--
+
+CREATE TABLE `board_game` (
+  `board_game_id` int(11) NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `designer` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image_path` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `min_age` int(11) DEFAULT NULL,
+  `players` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `playing_time` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `release_year` int(11) DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `board_game_category`
+--
+
+CREATE TABLE `board_game_category` (
+  `board_game_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
 --
 
 CREATE TABLE `role` (
@@ -34,7 +75,7 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Zrzut danych tabeli `role`
+-- Dumping data for table `role`
 --
 
 INSERT INTO `role` (`role_id`, `role`) VALUES
@@ -44,36 +85,36 @@ INSERT INTO `role` (`role_id`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
-  `login` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `active` int(11) NOT NULL,
   `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `active` tinyint(4) NOT NULL
+  `login` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Zrzut danych tabeli `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `login`, `email`, `password`, `first_name`, `last_name`, `active`) VALUES
-(1, 'admin', 'admin@gmail.com', '$2a$10$Pl.mEJvC/1zsg7FFgC5ihe0ddvPNeY3ag8S8Gm2E50sgH6sE8rh22', 'Admin', 'Admin', 1),
-(2, 'test', 'test@gmail.com', '$2a$10$TM92qxuC.39DpmnqX3hIxuQchUJIzUgh9h45JrU.3Alb.jzqgM5d2', 'Tester', 'Test', 1),
-(3, 'test2', 'test2@gmail.com', '$2a$10$TM92qxuC.39DpmnqX3hIxuQchUJIzUgh9h45JrU.3Alb.jzqgM5d2', 'Tester', 'Test', 1),
-(4, 'test3', 'test3@gmail.com', '$2a$10$TM92qxuC.39DpmnqX3hIxuQchUJIzUgh9h45JrU.3Alb.jzqgM5d2', 'Tester', 'Test', 1),
-(5, 'test4', 'test4@gmail.com', '$2a$10$TM92qxuC.39DpmnqX3hIxuQchUJIzUgh9h45JrU.3Alb.jzqgM5d2', 'Tester', 't', 1),
-(6, 'test5', 'test5@gmail.com', '$2a$10$rMQ3bRZF81x7glE4RWegauMihvnrnQw6T/Q./t.jEDMOQwF1mgB82', 'Tester', 'Test', 1),
-(7, 'test6', 'test6@gmail.com', '$2a$10$rMQ3bRZF81x7glE4RWegauMihvnrnQw6T/Q./t.jEDMOQwF1mgB82', 'Tester', 'Test', 1);
+INSERT INTO `user` (`user_id`, `active`, `email`, `first_name`, `last_name`, `login`, `password`) VALUES
+(1, 1, 'admin@gmail.com', 'Admin', 'Admin', 'admin', '$2a$10$Pl.mEJvC/1zsg7FFgC5ihe0ddvPNeY3ag8S8Gm2E50sgH6sE8rh22'),
+(2, 1, 'test@gmail.com', 'Tester', 'Test', 'test', '$2a$10$TM92qxuC.39DpmnqX3hIxuQchUJIzUgh9h45JrU.3Alb.jzqgM5d2'),
+(3, 1, 'test2@gmail.com', 'Tester', 'Test', 'test2', '$2a$10$TM92qxuC.39DpmnqX3hIxuQchUJIzUgh9h45JrU.3Alb.jzqgM5d2'),
+(4, 1, 'test3@gmail.com', 'Tester', 'Test', 'test3', '$2a$10$TM92qxuC.39DpmnqX3hIxuQchUJIzUgh9h45JrU.3Alb.jzqgM5d2'),
+(5, 0, 'test4@gmail.com', 'Tester', 'Test', 'test4', '$2a$10$TM92qxuC.39DpmnqX3hIxuQchUJIzUgh9h45JrU.3Alb.jzqgM5d2'),
+(6, 1, 'test5@gmail.com', 'Tester', 'Test', 'test5', '$2a$10$rMQ3bRZF81x7glE4RWegauMihvnrnQw6T/Q./t.jEDMOQwF1mgB82'),
+(7, 1, 'test6@gmail.com', 'Tester', 'Test', 'test6', '$2a$10$rMQ3bRZF81x7glE4RWegauMihvnrnQw6T/Q./t.jEDMOQwF1mgB82');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `user_role`
+-- Table structure for table `user_role`
 --
 
 CREATE TABLE `user_role` (
@@ -82,49 +123,105 @@ CREATE TABLE `user_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Zrzut danych tabeli `user_role`
+-- Dumping data for table `user_role`
 --
 
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
-(4, 2),
 (1, 1),
 (2, 2),
 (3, 2),
+(4, 2),
 (5, 2),
 (6, 2),
 (7, 2);
 
 --
--- Indeksy dla zrzutów tabel
+-- Indexes for dumped tables
 --
 
 --
--- Indeksy dla tabeli `role`
+-- Indexes for table `board_game`
+--
+ALTER TABLE `board_game`
+  ADD PRIMARY KEY (`board_game_id`);
+
+--
+-- Indexes for table `board_game_category`
+--
+ALTER TABLE `board_game_category`
+  ADD KEY `FKkrnqpjg7uys41j5rpuj50aynd` (`category_id`),
+  ADD KEY `FKset0ikuyeupb30084xlx3cf7n` (`board_game_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `role`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`role_id`);
 
 --
--- Indeksy dla tabeli `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`user_id`,`role_id`),
+  ADD KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `role`
+-- AUTO_INCREMENT for table `board_game`
+--
+ALTER TABLE `board_game`
+  MODIFY `board_game_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
   MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT dla tabeli `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `board_game_category`
+--
+ALTER TABLE `board_game_category`
+  ADD CONSTRAINT `FKkrnqpjg7uys41j5rpuj50aynd` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
+  ADD CONSTRAINT `FKset0ikuyeupb30084xlx3cf7n` FOREIGN KEY (`board_game_id`) REFERENCES `board_game` (`board_game_id`);
+
+--
+-- Constraints for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD CONSTRAINT `FK859n2jvi8ivhui0rl0esws6o` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `FKa68196081fvovjhkek5m97n3y` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
