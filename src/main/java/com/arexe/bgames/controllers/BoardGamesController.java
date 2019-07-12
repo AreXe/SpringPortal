41bd290fa10408d79715ee013fa7a898b19738b1
arexe <arexepro@gmail.com>
2019-07-12 23:19:19 +0200

    Add controller and view for admin board games list

---
+++ src/main/java/com/arexe/bgames/controllers/BoardGamesController.java
@@ -0,0 +1,39 @@
+package com.arexe.bgames.controllers;
+
+import com.arexe.bgames.entity.BoardGame;
+import com.arexe.bgames.service.BoardGameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class BoardGamesController {
+
+    private final BoardGameService boardGameService;
+
+    @Autowired
+    public BoardGamesController(BoardGameService boardGameService) {
+        this.boardGameService = boardGameService;
+    }
+
+    @GetMapping(value = "/admin/boardgames")
+    @Secured(value = {"ROLE_ADMIN"})
+    public String showAllBoardGames(Model model){
+        List<BoardGame> boardGamesList = boardGameService.findAllBoardGames();
+        model.addAttribute("bgList", boardGamesList);
+        return "admin/boardgames";
+    }
+
+    @GetMapping(value = "/admin/boardgames/search/{title}")
+    @Secured(value = {"ROLE_ADMIN"})
+    public String searchBoardGames(Model model, @PathVariable("title") String title){
+        List<BoardGame> boardGamesList = boardGameService.findBoardGamesByTitle(title);
+        model.addAttribute("bgList", boardGamesList);
+        return "admin/boardgames";
+    }
+}
--- src/main/webapp/WEB-INF/incl/anav.app
+++ src/main/webapp/WEB-INF/incl/anav.app
@@ -18,5 +18,14 @@
           </li>
 		</ul>
 
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Board games panel</span>
+        </h6>
+		<ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href="/admin/boardgames"><span data-feather="list"></span>Board games list</a>
+          </li>
+		</ul>
+
 		</div>
     </nav>
