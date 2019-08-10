a93dee36de607cc4dbdd4afe7a41231c1a0ec796
arexe <arexepro@gmail.com>
2019-08-10 19:31:31 +0200

    Add pagination for admin board games list

--- src/main/java/com/arexe/bgames/service/BoardGameService.java
+++ src/main/java/com/arexe/bgames/service/BoardGameService.java
@@ -1,12 +1,15 @@
 package com.arexe.bgames.service;
 
 import com.arexe.bgames.entity.BoardGame;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BoardGameService {
 
     List<BoardGame> findAllBoardGames();
+    Page<BoardGame> findAllBoardGamesPageable(Pageable pageable);
     List<BoardGame> findBoardGamesByTitle(String title);
     BoardGame findBoardGameById(Integer id);
     void saveBoardGame(BoardGame boardGame);
