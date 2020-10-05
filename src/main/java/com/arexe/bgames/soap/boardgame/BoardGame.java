52653cd7260ee67f0124526672f11a6f9eaac57d
arexe <arexepro@gmail.com>
2020-10-05 19:08:27 +0200

    Add BoardGame model and methods for SOAP-WS

---
+++ src/main/java/com/arexe/bgames/soap/boardgame/BoardGame.java
@@ -0,0 +1,136 @@
+package com.arexe.bgames.soap.boardgame;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * <p>Java class for boardGame complex type.
+ *
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ *
+ * <pre>
+ * <complexType name="boardGame">
+ *   <complexContent>
+ *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       <sequence>
+ *         <element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         <element name="title" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         <element name="designer" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         <element name="releaseYear" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         <element name="description" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       </sequence>
+ *     </restriction>
+ *   </complexContent>
+ * </complexType>
+ * </pre>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "boardGame", propOrder = {
+        "id",
+        "title",
+        "designer",
+        "releaseYear",
+        "description"
+})
+public class BoardGame {
+
+    protected int id;
+    @XmlElement(required = true)
+    protected String title;
+    @XmlElement(required = true)
+    protected String designer;
+    protected int releaseYear;
+    @XmlElement(required = true)
+    protected String description;
+
+    /**
+     * Gets the value of the id property.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * Gets the value of the title property.
+     *
+     * @return possible object is
+     * {@link String }
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the value of the title property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setTitle(String value) {
+        this.title = value;
+    }
+
+    /**
+     * Gets the value of the designer property.
+     *
+     * @return possible object is
+     * {@link String }
+     */
+    public String getDesigner() {
+        return designer;
+    }
+
+    /**
+     * Sets the value of the designer property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setDesigner(String value) {
+        this.designer = value;
+    }
+
+    /**
+     * Gets the value of the releaseYear property.
+     */
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    /**
+     * Sets the value of the releaseYear property.
+     */
+    public void setReleaseYear(int value) {
+        this.releaseYear = value;
+    }
+
+    /**
+     * Gets the value of the description property.
+     *
+     * @return possible object is
+     * {@link String }
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the value of the description property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+}
---
+++ src/main/java/com/arexe/bgames/soap/boardgame/GetBoardGameByIdRequest.java
@@ -0,0 +1,46 @@
+package com.arexe.bgames.soap.boardgame;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * <p>Java class for anonymous complex type.
+ *
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ *
+ * <pre>
+ * <complexType>
+ *   <complexContent>
+ *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       <sequence>
+ *         <element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       </sequence>
+ *     </restriction>
+ *   </complexContent>
+ * </complexType>
+ * </pre>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {"id"})
+@XmlRootElement(name = "getBoardGameByIdRequest")
+public class GetBoardGameByIdRequest {
+
+    protected int id;
+
+    /**
+     * Gets the value of the id property.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+}
